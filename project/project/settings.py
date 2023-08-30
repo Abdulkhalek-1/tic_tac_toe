453350f8dc978ad9b85466d022110ced59dad92c
High-2005 <highgamer2005@gmail.com>
2023-08-30 14:54:02 +0300

    database

--- project/project/settings.py
+++ project/project/settings.py
@@ -82,24 +82,17 @@ ASGI_APPLICATION = "project.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tic_tac_toe',
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#         'USER': 'postgres',
-#         'PASSWORD': 'qkzxnvhb2005'
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/ 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tic_tac_toe',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': 'qkzxnvhb2005'
     }
 }
+
 
 
 # Password validation
