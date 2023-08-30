765e087df8184d73e9b7127166e847101ea688e7
High-2005 <highgamer2005@gmail.com>
2023-08-30 14:07:45 +0300

    change to sqlite3

--- project/project/settings.py
+++ project/project/settings.py
@@ -82,17 +82,24 @@ ASGI_APPLICATION = "project.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tic_tac_toe',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#         'USER': 'postgres',
+#         'PASSWORD': 'qkzxnvhb2005'
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tic_tac_toe',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        'USER': 'postgres',
-        'PASSWORD': 'qkzxnvhb2005'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/ 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
